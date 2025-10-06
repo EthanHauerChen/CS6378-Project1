aaacf78c62d76b12662e977220b1151cae0de611
EthanHauerChen <ethanhchen01@gmail.com>
2025-10-06 17:07:26 -0500

    node

--- node.cpp
+++ node.cpp
@@ -1,10 +1,46 @@
 #include "node.h"
+#include <sys/socket.h>
+
+Node::Node(std::string hostname, int port) {
+    this.hostname = hostname;
+    this.port = port;
+}
 
 Node::listen_for_connections() { 
 
 }
 
 /** obtain list of hostnames and ports, create multiple sockets for each and attempt to connecdt */
-Node::initiate_connections(const std::string& hostnames, int[] ports /*prob not the right way to pass arrays*/) {
+Node::initiate_connections(const std::string& hostnames, int[] ports, int size) {
+    for (int i = 0; i < size; i++) {
+        int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        struct sockaddr_in address;
+        struct hostent * host;
+        
+        if (sockfd <= 0) {
+            fprintf(stderr, "error: unable to create socket\n");
+            return -1;
+        }
 
+        /* connect to server */
+        address.sin_family = AF_INET;
+        address.sin_port = htons(ports[i]);
+        host = gethostbyname(hostnames[i]);
+        
+        /* connection already exists, no need to connect */
+        if (address.sin_family = connections.contains)
+        if (!host) {
+            fprintf(stderr, "error: unknown host %s\n", argv[1]);
+            return -2;
+        }
+        memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
+        if (connect(sockfd, (struct sockaddr *)&address, sizeof(address))) {
+            fprintf(stderr, "%s: error: cannot connect to host %s\n", argv[0], argv[1]);
+            return -3;
+        }
+
+        connection_fds.insert(sockfd);
+    }
+
+    return 0; //success
 }
--- node.h
+++ node.h
@@ -2,16 +2,23 @@
 #define NODE_H
 #include <unordered_set>
 #include <string>
+#include <sys/socket.h>
+#include <vector>
 
 class Node {
-    //connection to other nodes, should be in the form of a list of file descriptors but obtained from sockets
-    std::unordered_set<int> connection_fds;
     std::string hostname;
     int port;
     bool isActive;
 
+    struct Connection {
+        int fd;
+        struct sockaddr_in address;
+    }
+
+    std::unordered_set<Connection> connections;
 
 public:
+    Node(std::string hostname, int port);
     int listen_for_connections();
     int initiate_connections();
     void become_active();
