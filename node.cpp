e5b9dba1406465d79a361786c68497106987ec47
Ethan <ethanhchen01@gmail.com>
2025-10-06 00:19:24 -0500

    node definition

---
+++ node.cpp
@@ -0,0 +1,10 @@
+#include "node.h"
+
+Node::listen_for_connections() { 
+
+}
+
+/** obtain list of hostnames and ports, create multiple sockets for each and attempt to connecdt */
+Node::initiate_connections(const std::string& hostnames, int[] ports /*prob not the right way to pass arrays*/) {
+
+}
--- node.h
+++ node.h
@@ -1,13 +1,21 @@
 #ifndef NODE_H
 #define NODE_H
 #include <unordered_set>
+#include <string>
 
 class Node {
     //connection to other nodes, should be in the form of a list of file descriptors but obtained from sockets
     std::unordered_set<int> connection_fds;
+    std::string hostname;
+    int port;
+    bool isActive;
 
-    template <typename Container> 
-    int initiateConnections(const auto& /*TODO*/)
+
+public:
+    int listen_for_connections();
+    int initiate_connections();
+    void become_active();
+    void become_passive();
 };
 
 #endif
