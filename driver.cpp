811c92b24b71ffced8f5165c8fa99474e1a663c0
Ethan <ethanhchen01@gmail.com>
2025-10-18 18:25:38 -0500

    test node setup

--- driver.cpp
+++ driver.cpp
@@ -9,4 +9,6 @@ int main(int argc, char ** argv) {
     struct config node_values;
     std::cout << "error: " << extract_config(argv[1], node_values) << "\n";
     std::cout << node_values;
+    Node n {node_values};
+    
 }
