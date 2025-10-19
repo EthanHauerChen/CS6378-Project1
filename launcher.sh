3d561e8023680b3fcc88b90e3e2654e158a01c81
Ethan <ethanhchen01@gmail.com>
2025-10-18 22:26:20 -0500

    empty shell file

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 
-CONFIG_FILE="config.txt"
-MAIN_CLASS="./launcher"
-PROJECT_DIRECTORY="."
-
-num_nodes=$(awk 'NR==1 {print $1}' "$CONFIG_FILE")
-
-for ((i=0; i<num_nodes; i++)); do
-  echo "Launching Node $i"
-
-  cmd.exe /c start cmd.exe /k "cd /d $PROJECT_DIRECTORY && $MAIN_CLASS $CONFIG_FILE"
-done
