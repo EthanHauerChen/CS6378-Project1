20670f94ad391118acc57ed88c0232acfa6e90f9
Ethan <ethanhchen01@gmail.com>
2025-10-21 23:13:25 -0500

    debug extract_clock

--- node.cpp
+++ node.cpp
@@ -209,7 +209,7 @@ bool Node::read_nonblocking(int fd, std::string& buf, size_t count) {
 std::vector<int> Node::extract_clock(std::string msg) {
     std::vector<std::string> tokens = split(msg, " ");
     std::vector<int> clock_vals(tokens.size() - 1);
-    std::cout << "message to extract: " << msg << ". is the last char a space: " << msg.back() << " == " << "\" \"" << msg.back() == ' ' << "\n";
+    std::cout << "message to extract: " << msg << ". is the last char a space: " << (msg.back() << " == " << "\" \"") << msg.back() == ' ' << "\n";
     std::cout << "tokens size: " << tokens.size() << "\ntokens: " << std::flush;
     for (int i = 1; i < tokens.size(); i++) { //first token is msg_type, not part of vector clock
         std::cout << tokens[i] << ", ";
