96cbce708cd6f68f83d488290298d228399183d1
EthanHauerChen <ethanhchen01@gmail.com>
2025-10-30 19:59:47 -0500

    bring back a debug msg. if working properly, should not clog up screen. if not working properly, may clog up screen

--- node.cpp
+++ node.cpp
@@ -203,7 +203,7 @@ std::string Node::read_msg(int fd) {
     len = ntohl(len);
     size_t total_read = 0;
     char buffer[len];
-    //std::cout << "len is " << len << "\n" << std::flush;
+    std::cout << "len is " << len << "\n" << std::flush;
     if (returnval == 0) { //socket connection closed, abort
         int nodenum = -1; 
         for (const auto& p : this->connections) { //obtain nodenum
@@ -212,7 +212,7 @@ std::string Node::read_msg(int fd) {
                 break;
             }
         }
-        std::cerr << "socket connection with " << nodenum << " closed. aborting" << " len is " << len << "\n";
+        std::cerr << "socket connection with " << nodenum << " closed. aborting" << " len is " << len << "\n" << std::flush;
         this->destroy = true;
         return "";
     }
