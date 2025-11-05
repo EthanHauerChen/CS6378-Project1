098a6644df1c65933f86bebaf00040e2b05e9c3a
EthanHauerChen <ethanhchen01@gmail.com>
2025-11-05 16:05:32 -0600

    wait longer to hear back from neighbors

--- node.cpp
+++ node.cpp
@@ -344,7 +344,7 @@ bool Node::start_MAP() { //startup function, consists of ensuring all nodes are 
             }
             else if (msg == "2") return false;
         }
-        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - past).count() > 8) {
+        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - past).count() > 13) {
             std::cerr << "Node " << this->node_number << " failure to hear back from all neighbors, abort\n";
             return false; 
         }
